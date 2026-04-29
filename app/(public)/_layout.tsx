89283c9ff4085b82731bf713aba695a8b2895e42
duong <ngoquyduonghn2005@gmail.com>
2026-04-29 18:39:20 +1000

    Fix

---
+++ app/(public)/_layout.tsx
@@ -0,0 +1,10 @@
+// app/(public)/_layout.tsx
+import { Stack } from "expo-router";
+
+export default function PublicLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
