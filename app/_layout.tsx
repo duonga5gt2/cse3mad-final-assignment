8b7b28e1210c17703ff10c69aceaeab2d66f9898
duong <ngoquyduonghn2005@gmail.com>
2026-04-29 18:29:21 +1000

    Fix app/_layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -23,7 +23,7 @@ function AppContent() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(public)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen
           name="modal"
